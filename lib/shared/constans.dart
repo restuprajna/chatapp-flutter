df7ae0071eaa1ca75bb23915cae01cd8abcc9e84
Restu Prajna <restuprajna@Restus-MacBook-Air.local>
2023-08-28 21:44:19 +0800

    first commit login page finished

---
+++ lib/shared/constans.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+
+class Constants {
+  static String appId = "1:387033446850:web:051ebfbb8f6a82f3cb7e5b";
+  static String apiKey = "AIzaSyBdR-SfrKMGdF5IownA1yo49A6Bn1v8qnU";
+  static String messagingSenderId = "387033446850";
+  static String projectId = "chatappflutter-d3334";
+  final primaryColor = Colors.pink[300];
+}
