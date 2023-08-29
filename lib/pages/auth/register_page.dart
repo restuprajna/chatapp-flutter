a899a1c0a1437129ff8b0fa4725001bacc873c73
Restu Prajna <restuprajna@Restus-MacBook-Air.local>
2023-08-29 09:45:21 +0800

    register page ui finished

--- lib/pages/auth/register_page.dart
+++ lib/pages/auth/register_page.dart
@@ -1,3 +1,6 @@
+import 'package:chatapp_firebase/pages/auth/login_page.dart';
+import 'package:chatapp_firebase/widgets/widgets.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
 
 class RegisterPage extends StatefulWidget {
@@ -8,12 +11,147 @@ class RegisterPage extends StatefulWidget {
 }
 
 class _RegisterPageState extends State<RegisterPage> {
+  final formKey = GlobalKey<FormState>();
+  String email = "";
+  String password = "";
+  String fullName = "";
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
-      body: Center(
-        child: Text("Register Page"),
+    return Scaffold(
+        body: SingleChildScrollView(
+      child: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 80),
+        child: Form(
+            key: formKey,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                const Text(
+                  "MeeChat",
+                  style: TextStyle(fontSize: 40, fontWeight: FontWeight.bold),
+                ),
+                const SizedBox(
+                  height: 10,
+                ),
+                const Text(
+                  "Join now and feel the pleasure",
+                  style: TextStyle(fontSize: 15, fontWeight: FontWeight.w400),
+                ),
+                Image.asset("assets/images/register.png"),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Full Name",
+                      prefixIcon: Icon(
+                        Icons.person,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      fullName = val;
+                    });
+                  },
+
+                  // check the validation
+                  validator: (val) {
+                    if (val!.isEmpty) {
+                      return "Name cannot be empty.";
+                    } else {
+                      return null;
+                    }
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Email",
+                      prefixIcon: Icon(
+                        Icons.email,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  onChanged: (val) {
+                    setState(() {
+                      email = val;
+                    });
+                  },
+
+                  // check the validation
+                  validator: (val) {
+                    return RegExp(
+                                r"^[a-zA-Z0-9.a-zA-Z0-9.!#$%&'*+-/=?^_`{|}~]+@[a-zA-Z0-9]+\.[a-zA-Z]+")
+                            .hasMatch(val!)
+                        ? null
+                        : "Please enter a valid email";
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                TextFormField(
+                  obscureText: true,
+                  decoration: textInputDecoration.copyWith(
+                      labelText: "Password",
+                      prefixIcon: Icon(
+                        Icons.lock,
+                        color: Theme.of(context).primaryColor,
+                      )),
+                  validator: (val) {
+                    if (val!.length < 6) {
+                      return "Password must be at least 6 character long";
+                    } else {
+                      return null;
+                    }
+                  },
+                  onChanged: (val) {
+                    setState(() {
+                      password = val;
+                    });
+                  },
+                ),
+                const SizedBox(
+                  height: 20,
+                ),
+                SizedBox(
+                    width: double.infinity,
+                    child: ElevatedButton(
+                      onPressed: () {
+                        register();
+                      },
+                      style: ElevatedButton.styleFrom(
+                          backgroundColor: Theme.of(context).primaryColor,
+                          elevation: 0,
+                          shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(30))),
+                      child: const Text("Sign up",
+                          style: TextStyle(color: Colors.white, fontSize: 16)),
+                    )),
+                const SizedBox(
+                  height: 10,
+                ),
+                Text.rich(TextSpan(
+                    text: "Already have an account? ",
+                    style: const TextStyle(color: Colors.black, fontSize: 14),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: "login here",
+                          style: const TextStyle(
+                              color: Colors.black,
+                              decoration: TextDecoration.underline),
+                          recognizer: TapGestureRecognizer()
+                            ..onTap = () {
+                              nextScreenReplace(context, const LoginPage());
+                            })
+                    ]))
+              ],
+            )),
       ),
-    );
+    ));
+  }
+
+  register() {
+    if (formKey.currentState!.validate()) {}
   }
 }
