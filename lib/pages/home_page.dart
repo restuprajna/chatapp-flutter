d0daf17e631949066c10839053b54c5295957b10
Restu Prajna <restuprajna@Restus-MacBook-Air.local>
2023-09-18 09:08:02 +0800

    create profile page

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -96,7 +96,12 @@ class _HomePageState extends State<HomePage> {
             ),
             ListTile(
               onTap: () {
-                nextScreen(context, const ProfilePage());
+                nextScreenReplace(
+                    context,
+                    ProfilePage(
+                      userName: userName,
+                      email: email,
+                    ));
               },
               contentPadding:
                   const EdgeInsets.symmetric(horizontal: 20, vertical: 5),
